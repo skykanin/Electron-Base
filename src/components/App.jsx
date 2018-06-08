da76ced381a92506182c66c1bb1caf542afcec5d
skykanin <3789764+skykanin@users.noreply.github.com>
2018-06-09 00:10:58 +0200

    removed flow comment from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
-// @flow
 import React from 'react';
-
 import {hot} from 'react-hot-loader';
-
 import styled from 'styled-components';
 
 const Title = styled.h1`
@@ -16,16 +13,12 @@ const Wrapper = styled.section`
   padding: 4em;
 `;
 
-const x = "Hello";
-
-console.log(x);
-
 const App = () => {
   return(
     <Wrapper>
-    <Title>
-      Hello World, this is my first styled component!
-    </Title>
+      <Title>
+        Hello World, this is my first styled component!
+      </Title>
   </Wrapper>
   );
 };

ca7c523f7583886083d54c222c2b13fe0c77b4e4
skykanin <3789764+skykanin@users.noreply.github.com>
2018-06-08 19:31:19 +0200

    added styled components support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 import {hot} from 'react-hot-loader';
 
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  background: papayawhip;
+  padding: 4em;
+`;
+
 const App = () => {
-  return <div>Hello React! This is a test</div>;
+  return(
+    <Wrapper>
+    <Title>
+      Hello World, this is my first styled component!
+    </Title>
+  </Wrapper>
+  );
 };
 
 export default hot(module)(App);

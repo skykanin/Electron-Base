5116566ff84c642bdcb34b2176c085cb67e35ee0
skykanin <3789764+skykanin@users.noreply.github.com>
2018-06-08 17:32:38 +0200

    added jest support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {hot} from 'react-hot-loader';
 
 const App = () => {
-  return <div>Hello React!</div>;
+  return <div>Hello React! This is a test</div>;
 };
 
 export default hot(module)(App);
